64f7917b46c61634976c28e491b4aa3413f1e4da
Jairo <jespinozac96@gmail.com>
2025-11-27 21:24:15 -0500

    primer commit app Bruma

---
+++ src/test/java/com/pe/Bruma/BrumaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.Bruma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrumaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
