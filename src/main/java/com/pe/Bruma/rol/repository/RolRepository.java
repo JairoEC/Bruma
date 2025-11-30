cce0d517493cbafbe0d82fdefd3c447d18df7d78
Rlizanov <omarlivill@gmail.com>
2025-11-29 23:50:13 -0500

    Modificación de RolRepository: cambio de tipo de id a Long y renombramiento del método existByNombre a existsByNombreIgnoreCase

--- src/main/java/com/pe/Bruma/rol/repository/RolRepository.java
+++ src/main/java/com/pe/Bruma/rol/repository/RolRepository.java
@@ -3,10 +3,10 @@ package com.pe.Bruma.rol.repository;
 import com.pe.Bruma.rol.entity.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RolRepository extends JpaRepository <Rol, Integer >{
+public interface RolRepository extends JpaRepository <Rol, Long >{
 
 //METODOS PERSONALIZADOS
-    Boolean existByNombre(String nombre);
+    boolean existsByNombreIgnoreCase(String nombre);
 
 
 }
