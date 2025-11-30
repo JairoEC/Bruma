5548128c8e821c9f94eb79dda60178d1638b4150
Rlizanov <omarlivill@gmail.com>
2025-11-29 23:51:42 -0500

    Creación de la interfaz RolService con métodos para gestionar roles

---
+++ src/main/java/com/pe/Bruma/rol/service/RolService.java
@@ -0,0 +1,17 @@
+package com.pe.Bruma.rol.service;
+
+import com.pe.Bruma.rol.api.request.RolCreateRequestDto;
+import com.pe.Bruma.rol.api.request.RolUpdateRequestDto;
+import com.pe.Bruma.rol.api.response.RolResponseDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface RolService {
+    RolResponseDto createRol(RolCreateRequestDto dto);
+    List<RolResponseDto> getAllRol();
+    RolResponseDto getRoles(Long id);
+    void deleteRol(Long id);
+    RolResponseDto updateRol(Long id, RolUpdateRequestDto dto);
+}
