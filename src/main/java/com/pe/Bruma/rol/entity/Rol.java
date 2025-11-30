aa2cb7148b4a194cc1bbfc1c0caecd9fb5859395
Rlizanov <omarlivill@gmail.com>
2025-11-29 23:49:31 -0500

    Modificación de la entidad Rol: cambio de tipo de id a Long y adición del campo estado

--- src/main/java/com/pe/Bruma/rol/entity/Rol.java
+++ src/main/java/com/pe/Bruma/rol/entity/Rol.java
@@ -9,8 +9,11 @@ import lombok.Data;
 public class Rol {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "rol_id")
-    private Integer id;
+    private Long id;
 
     @Column(name = "nombre", nullable = false, length = 50, unique = true)
     private String nombre;
+
+    @Column(name = "estado", nullable = false, length = 20)
+    private String estado;
 }
