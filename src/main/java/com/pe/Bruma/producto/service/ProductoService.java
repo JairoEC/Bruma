7296a9f8ba4e043266b005a28d0797a0df763459
Alisoooon <anjelaalison07@gmail.com>
2025-11-30 01:22:04 -0500

    controllador y service de producto

---
+++ src/main/java/com/pe/Bruma/producto/service/ProductoService.java
@@ -0,0 +1,39 @@
+package com.pe.Bruma.producto.service;
+
+import com.pe.Bruma.producto.entity.Producto;
+import com.pe.Bruma.producto.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository repo;
+
+    public List<Producto> listar() {
+        return repo.findAll();
+    }
+
+    public Producto buscarPorId(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Producto registrar(Producto producto) {
+        return repo.save(producto);
+    }
+
+    public Producto actualizar(Producto producto) {
+        return repo.save(producto);
+    }
+
+    public void eliminar(Long id) {
+        repo.deleteById(id);
+    }
+
+    public List<Producto> buscarPorNombre(String texto) {
+        return repo.buscarPorNombre(texto);
+    }
+}
