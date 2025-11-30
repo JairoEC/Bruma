8f49fc2aa60156a5ce0c326edd81f1db6238603a
Rlizanov <omarlivill@gmail.com>
2025-11-30 10:24:15 -0500

    Implementar método updateEmpleado para actualizar empleado y rol

--- src/main/java/com/pe/Bruma/empleado/service/EmpleadoServiceImpl.java
+++ src/main/java/com/pe/Bruma/empleado/service/EmpleadoServiceImpl.java
@@ -6,6 +6,7 @@ import com.pe.Bruma.empleado.api.response.EmpleadoResponseDto;
 import com.pe.Bruma.empleado.entity.Empleado;
 import com.pe.Bruma.empleado.mapper.EmpleadoMapper;
 import com.pe.Bruma.empleado.repository.EmpleadoRepository;
+import com.pe.Bruma.rol.entity.Rol;
 import com.pe.Bruma.rol.repository.RolRepository;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -47,7 +48,17 @@ public class EmpleadoServiceImpl implements EmpleadoService {
 
     @Override
     public EmpleadoResponseDto updateEmpleado(Long id, EmpleadoUpdateRequestDto dto) {
-        return null;
+        Empleado empleadoActual = empleadoRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("El empleado con id '" + id + "' no existe."));
+
+
+        Rol nuevoRol = rolRepo.findById(dto.getRolId())
+                .orElseThrow(() -> new IllegalArgumentException("El rol con id '" + dto.getRolId() + "' no existe."));
+
+        empleadoActual.setRol(nuevoRol);
+        empleadoMapper.updateEntityFromDto(dto, empleadoActual);
+        Empleado empleadoActualizado = empleadoRepo.save(empleadoActual);
+        return empleadoMapper.toResponseDto(empleadoActualizado);
     }
 
 
